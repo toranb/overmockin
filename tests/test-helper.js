5a234104229b4d3e567b12c55bc049a80bcc5ab9
toran billups <toranb@gmail.com>
2018-03-21 19:37:29 -0500

    drop a few legacy ember qunit test helpers

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,14 +1,12 @@
 import Application from '../app';
-import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-
 import LinkComponent from '@ember/routing/link-component';
 
 LinkComponent.reopen({
   attributeBindings: ['test-id']
 });
 
-setApplication(Application.create(config.APP));
+setApplication(Application.create({ autoboot: false }));
 
 start();
