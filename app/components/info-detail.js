3f051f3ad490fdc653fe6310196c91dda98d2804
toran billups <toranb@gmail.com>
2018-03-22 06:43:36 -0500

    convert info-detail to es2015 class

--- app/components/info-detail.js
+++ app/components/info-detail.js
@@ -1,18 +1,21 @@
 import { connect } from 'ember-redux';
 import { all, activeItem } from '../reducers/information';
-import { computed } from '@ember/object';
 import Component from '@ember/component';
+import { computed } from '@ember-decorators/object';
 
 const stateToComputed = state => ({
   items: all(state),
   item: activeItem(state)
 });
 
-const InfoDetailComponent = Component.extend({
-  width: computed(function() {
+class InfoDetailComponent extends Component {
+
+  @computed
+  get width() {
     const items = this.get('items');
     return items ? (Object.keys(items).length / 10) * 100 : 0;
-  })
-});
+  }
+
+}
 
 export default connect(stateToComputed)(InfoDetailComponent);
